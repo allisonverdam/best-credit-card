0c72d8a0d7d83613a76b10b69aca9d66c07341a1
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-10-07 23:34:08 -0300

    Definindo models

---
+++ models/card.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+
+	"github.com/go-ozzo/ozzo-validation"
+)
+
+type Card struct {
+	Id             int       `json:"id" db:"id"`
+	Number         string    `json:"number" db:"cc_number"`
+	DueDate        time.Time `json:"due_date" db:"cc_due_date"`
+	ExpirationDate time.Time `json:"expiration_date" db:"cc_expiration_date"`
+	CVV            int       `json:"cvv" db:"cc_cvv"`
+	Limit          float64   `json:"limit" db:"cc_limit"`
+	PersonId       int       `json:"person_id" db:"person_id"`
+}
+
+func (m Card) Validate() error {
+	return validation.ValidateStruct(&m,
+		validation.Field(&m.Number, validation.Required, validation.Length(16, 16)),
+	)
+}
---
+++ models/person.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"github.com/go-ozzo/ozzo-validation"
+)
+
+type Person struct {
+	Id       int    `json:"id" db:"id"`
+	Name     string `json:"name" db:"name"`
+	Username string `json:"username" db:"username"`
+	Email    string `json:"email" db:"email"`
+	Password string `json:"password" db:"password"`
+}
+
+func (p Person) Validate() error {
+	return validation.ValidateStruct(&p,
+		validation.Field(&p.Username, validation.Required, validation.Length(0, 20)),
+		validation.Field(&p.Password, validation.Required, validation.Length(8, 20)),
+	)
+}
