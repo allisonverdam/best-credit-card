607fcb04907f43b4e91c65dbc1736ece85acb502
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-10-15 16:48:12 -0200

    Criando a carteira

---
+++ models/wallet.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"github.com/go-ozzo/ozzo-validation"
+)
+
+type Wallet struct {
+	Id           int     `json:"id" db:"id"`
+	RealLimit    float64 `json:"real_limit" db:"real_limit"`
+	MaximumLimit float64 `json:"maximum_limit" db:"maximum_limit"`
+	PersonId     int     `json:"person_id" db:"person_id"`
+}
+
+func (p Wallet) Validate() error {
+	return validation.ValidateStruct(&p,
+		validation.Field(&p.PersonId, validation.Required),
+	)
+}
