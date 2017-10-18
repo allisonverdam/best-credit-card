45461031ea2e03be40698ee971ada4934b0568b9
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-10-17 23:32:06 -0200

    alterando dados de teste

--- testdata/db.sql
+++ testdata/db.sql
@@ -44,7 +44,7 @@ INSERT INTO wallet (real_limit, maximum_limit, person_id) VALUES (0, 0, 2);
 INSERT INTO wallet (real_limit, maximum_limit, person_id) VALUES (0, 0, 3);
 
 INSERT INTO card (cc_number, cc_due_date, cc_expiration_date, cc_cvv, cc_limit, wallet_id) VALUES ('1234123412341230', '2016/01/12', '2017/10/12', 123, '300', 1);
-INSERT INTO card (cc_number, cc_due_date, cc_expiration_date, cc_cvv, cc_limit, wallet_id) VALUES ('1234123412341231', '2016/02/11', '2017/10/12', 123, '400', 1);
+INSERT INTO card (cc_number, cc_due_date, cc_expiration_date, cc_cvv, cc_limit, wallet_id) VALUES ('1234123412341231', '2016/01/12', '2017/10/12', 123, '400', 1);
 INSERT INTO card (cc_number, cc_due_date, cc_expiration_date, cc_cvv, cc_limit, wallet_id) VALUES ('1234123412341232', '2016/03/10', '2017/10/12', 123, '500', 1);
 INSERT INTO card (cc_number, cc_due_date, cc_expiration_date, cc_cvv, cc_limit, wallet_id) VALUES ('1234123412341233', '2016/04/09', '2017/10/12', 123, '600', 2);
 INSERT INTO card (cc_number, cc_due_date, cc_expiration_date, cc_cvv, cc_limit, wallet_id) VALUES ('1234123412341234', '2016/05/08', '2017/10/12', 123, '300', 2);
