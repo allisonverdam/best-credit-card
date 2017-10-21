65461c4fb7bdf1478466bc07a336da9afed9b163
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-10-20 23:29:22 -0200

    Removendo metodos desnecessarios

--- controllers/person.go
+++ controllers/person.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"strconv"
-
 	"github.com/allisonverdam/best-credit-card/app"
 	"github.com/allisonverdam/best-credit-card/models"
 	routing "github.com/go-ozzo/ozzo-routing"
@@ -12,12 +10,7 @@ type (
 	// personService especifica a interface que é utilizada pelo personResource.
 	personService interface {
 		Get(rs app.RequestScope, id int) (*models.Person, error)
-		GetPersonByUserName(rs app.RequestScope, username string) (*models.Person, error)
-		Query(rs app.RequestScope, offset, limit int) ([]models.Person, error)
-		Count(rs app.RequestScope) (int, error)
-		Create(rs app.RequestScope, model *models.Person) (*models.Person, error)
-		Update(rs app.RequestScope, id int, model *models.Person) (*models.Person, error)
-		Delete(rs app.RequestScope, id int) (*models.Person, error)
+		Update(rs app.RequestScope, id int, person *models.Person) (*models.Person, error)
 	}
 
 	// personResource define os handlers para as chamadas do controller.
@@ -29,58 +22,12 @@ type (
 // ServePersonResource define as rotas
 func ServePersonResource(rg *routing.RouteGroup, service personService) {
 	r := &personResource{service}
-	rg.Get("/persons/<id>", r.get)
-	rg.Get("/persons/<username>", r.GetPersonByUserName)
-	rg.Get("/persons", r.query)
-	rg.Post("/persons", r.create)
-	rg.Put("/persons/<id>", r.update)
-	rg.Delete("/persons/<id>", r.delete)
+	rg.Get("/me", r.get)
+	rg.Put("/me", r.update)
 }
 
 func (r *personResource) get(c *routing.Context) error {
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		return err
-	}
-
-	response, err := r.service.Get(app.GetRequestScope(c), id)
-	if err != nil {
-		return err
-	}
-
-	return c.Write(response)
-}
-
-func (r *personResource) GetPersonByUserName(c *routing.Context) error {
-	response, err := r.service.GetPersonByUserName(app.GetRequestScope(c), c.Param("username"))
-	if err != nil {
-		return err
-	}
-
-	return c.Write(response)
-}
-
-func (r *personResource) query(c *routing.Context) error {
-	rs := app.GetRequestScope(c)
-	count, err := r.service.Count(rs)
-	if err != nil {
-		return err
-	}
-	paginatedList := getPaginatedListFromRequest(c, count)
-	items, err := r.service.Query(app.GetRequestScope(c), paginatedList.Offset(), paginatedList.Limit())
-	if err != nil {
-		return err
-	}
-	paginatedList.Items = items
-	return c.Write(paginatedList)
-}
-
-func (r *personResource) create(c *routing.Context) error {
-	var model models.Person
-	if err := c.Read(&model); err != nil {
-		return err
-	}
-	response, err := r.service.Create(app.GetRequestScope(c), &model)
+	response, err := r.service.Get(app.GetRequestScope(c), app.GetRequestScope(c).UserID())
 	if err != nil {
 		return err
 	}
@@ -89,37 +36,18 @@ func (r *personResource) create(c *routing.Context) error {
 }
 
 func (r *personResource) update(c *routing.Context) error {
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		return err
-	}
-
 	rs := app.GetRequestScope(c)
 
-	model, err := r.service.Get(rs, id)
+	person, err := r.service.Get(rs, rs.UserID())
 	if err != nil {
 		return err
 	}
 
-	if err := c.Read(model); err != nil {
+	if err := c.Read(person); err != nil {
 		return err
 	}
 
-	response, err := r.service.Update(rs, id, model)
-	if err != nil {
-		return err
-	}
-
-	return c.Write(response)
-}
-
-func (r *personResource) delete(c *routing.Context) error {
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		return err
-	}
-
-	response, err := r.service.Delete(app.GetRequestScope(c), id)
+	response, err := r.service.Update(rs, rs.UserID(), person)
 	if err != nil {
 		return err
 	}
