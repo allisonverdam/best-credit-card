bdcd16e11def14c6dea3ea482c844f71eb457114
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-10-20 23:29:37 -0200

    Removendo metodos desnecessarios

--- services/person.go
+++ services/person.go
@@ -9,18 +9,10 @@ import (
 type personDAO interface {
 	// Get returns the person with the specified person ID.
 	Get(rs app.RequestScope, id int) (*models.Person, error)
-	// Get returns the person with the specified person username.
-	GetPersonByUserName(rs app.RequestScope, username string) (*models.Person, error)
-	// Count returns the number of persons.
-	Count(rs app.RequestScope) (int, error)
-	// Query returns the list of persons with the given offset and limit.
-	Query(rs app.RequestScope, offset, limit int) ([]models.Person, error)
-	// Create saves a new person in the storage.
-	Create(rs app.RequestScope, person *models.Person) error
 	// Update updates the person with given ID in the storage.
 	Update(rs app.RequestScope, id int, person *models.Person) error
-	// Delete removes the person with given ID from the storage.
-	Delete(rs app.RequestScope, id int) error
+	// Create saves a new person in the storage.
+	Create(rs app.RequestScope, person *models.Person) error
 }
 
 // PersonService provides services related with persons.
@@ -38,49 +30,32 @@ func (s *PersonService) Get(rs app.RequestScope, id int) (*models.Person, error)
 	return s.dao.Get(rs, id)
 }
 
-// Get returns the person with the specified the person username.
-func (s *PersonService) GetPersonByUserName(rs app.RequestScope, username string) (*models.Person, error) {
-	return s.dao.GetPersonByUserName(rs, username)
-}
-
-// Create creates a new person.
-func (s *PersonService) Create(rs app.RequestScope, model *models.Person) (*models.Person, error) {
-	if err := model.Validate(); err != nil {
-		return nil, err
-	}
-	if err := s.dao.Create(rs, model); err != nil {
-		return nil, err
-	}
-	return s.dao.Get(rs, model.Id)
-}
-
 // Update updates the person with the specified ID.
-func (s *PersonService) Update(rs app.RequestScope, id int, model *models.Person) (*models.Person, error) {
-	if err := model.Validate(); err != nil {
+func (s *PersonService) Update(rs app.RequestScope, id int, person *models.Person) (*models.Person, error) {
+	if err := person.ValidateOnUpdate(); err != nil {
 		return nil, err
 	}
-	if err := s.dao.Update(rs, id, model); err != nil {
+
+	oldPerson, err := s.dao.Get(rs, id)
+	if err != nil {
+		return nil, err
+	}
+
+	person.Password = oldPerson.Password
+
+	if err := s.dao.Update(rs, id, person); err != nil {
 		return nil, err
 	}
 	return s.dao.Get(rs, id)
 }
 
-// Delete deletes the person with the specified ID.
-func (s *PersonService) Delete(rs app.RequestScope, id int) (*models.Person, error) {
-	person, err := s.dao.Get(rs, id)
-	if err != nil {
+// Create creates a new person.
+func (s *PersonService) Create(rs app.RequestScope, person *models.Person) (*models.Person, error) {
+	if err := person.Validate(); err != nil {
 		return nil, err
 	}
-	err = s.dao.Delete(rs, id)
-	return person, err
-}
-
-// Count returns the number of persons.
-func (s *PersonService) Count(rs app.RequestScope) (int, error) {
-	return s.dao.Count(rs)
-}
-
-// Query returns the persons with the specified offset and limit.
-func (s *PersonService) Query(rs app.RequestScope, offset, limit int) ([]models.Person, error) {
-	return s.dao.Query(rs, offset, limit)
+	if err := s.dao.Create(rs, person); err != nil {
+		return nil, err
+	}
+	return s.dao.Get(rs, person.Id)
 }
