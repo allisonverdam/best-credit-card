850c9c32d73cf2c9237af8a3994a364897dbbd7c
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-10-22 23:35:03 -0200

    removendo paginacao e melhorando validacao da wallet

--- services/wallet.go
+++ services/wallet.go
@@ -9,10 +9,6 @@ import (
 type WalletDAO interface {
 	// Get returns the wallet with the specified wallet ID.
 	Get(rs app.RequestScope, id int) (*models.Wallet, error)
-	// Count returns the number of wallets.
-	Count(rs app.RequestScope) (int, error)
-	// Query returns the list of wallets with the given offset and limit.
-	Query(rs app.RequestScope, offset, limit int) ([]models.Wallet, error)
 	// Create saves a new wallet in the storage.
 	Create(rs app.RequestScope, wallet *models.Wallet) error
 	// Update updates the wallet with given ID in the storage.
@@ -37,22 +33,22 @@ func (s *WalletService) Get(rs app.RequestScope, id int) (*models.Wallet, error)
 }
 
 // Create creates a new wallet.
-func (s *WalletService) Create(rs app.RequestScope, model *models.Wallet) (*models.Wallet, error) {
-	if err := model.Validate(); err != nil {
+func (s *WalletService) Create(rs app.RequestScope, wallet *models.Wallet) (*models.Wallet, error) {
+	if err := wallet.Validate(); err != nil {
 		return nil, err
 	}
-	if err := s.dao.Create(rs, model); err != nil {
+	if err := s.dao.Create(rs, wallet); err != nil {
 		return nil, err
 	}
-	return s.dao.Get(rs, model.Id)
+	return s.dao.Get(rs, wallet.Id)
 }
 
 // Update updates the wallet with the specified ID.
-func (s *WalletService) Update(rs app.RequestScope, id int, model *models.Wallet) (*models.Wallet, error) {
-	if err := model.Validate(); err != nil {
+func (s *WalletService) Update(rs app.RequestScope, id int, wallet *models.Wallet) (*models.Wallet, error) {
+	if err := wallet.Validate(); err != nil {
 		return nil, err
 	}
-	if err := s.dao.Update(rs, id, model); err != nil {
+	if err := s.dao.Update(rs, id, wallet); err != nil {
 		return nil, err
 	}
 	return s.dao.Get(rs, id)
@@ -66,14 +62,4 @@ func (s *WalletService) Delete(rs app.RequestScope, id int) (*models.Wallet, err
 	}
 	err = s.dao.Delete(rs, id)
 	return wallet, err
-}
-
-// Count returns the number of wallets.
-func (s *WalletService) Count(rs app.RequestScope) (int, error) {
-	return s.dao.Count(rs)
-}
-
-// Query returns the wallets with the specified offset and limit.
-func (s *WalletService) Query(rs app.RequestScope, offset, limit int) ([]models.Wallet, error) {
-	return s.dao.Query(rs, offset, limit)
 }
