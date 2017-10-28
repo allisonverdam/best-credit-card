fbdcf705c36dee604b5735eb7a1bf485431da464
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-10-27 22:00:40 -0200

    Alterando nome de alguns dos testes para ficar mais facil distingui-los

--- daos/wallet_test.go
+++ daos/wallet_test.go
@@ -9,99 +9,97 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWalletDAO(t *testing.T) {
+func TestGetWallet(t *testing.T) {
 	db := testdata.ResetDB()
 	dao := NewWalletDAO()
 
-	{
-		// Get
-		testDBCall(db, func(rs app.RequestScope) {
-			wallet, err := dao.Get(rs, 1)
-			assert.Nil(t, err)
-			if assert.NotNil(t, wallet) {
-				assert.Equal(t, 1, wallet.Id)
-			}
-		})
-	}
+	testDBCall(db, func(rs app.RequestScope) {
+		wallet, err := dao.Get(rs, 1)
+		assert.Nil(t, err)
+		if assert.NotNil(t, wallet) {
+			assert.Equal(t, 1, wallet.Id)
+		}
+	})
+}
 
-	{
-		// Get
-		testDBCall(db, func(rs app.RequestScope) {
-			wallets, err := dao.GetAuthenticatedPersonWallets(rs, rs.UserID())
-			assert.Nil(t, err)
-			if assert.NotNil(t, wallets) {
-				assert.Equal(t, 1, wallets[0].Id)
-			}
-		})
-	}
+func TestGetWalletOfAuthenticatedPerson(t *testing.T) {
+	db := testdata.ResetDB()
+	dao := NewWalletDAO()
 
-	{
-		// Create
-		testDBCall(db, func(rs app.RequestScope) {
-			wallet := &models.Wallet{
-				MaximumLimit: 500,
-				RealLimit:    200,
-			}
-			err := dao.Create(rs, wallet)
-			assert.Nil(t, err)
-			assert.NotZero(t, wallet.Id)
-		})
-	}
+	testDBCall(db, func(rs app.RequestScope) {
+		wallets, err := dao.GetAuthenticatedPersonWallets(rs, rs.UserID())
+		assert.Nil(t, err)
+		if assert.NotNil(t, wallets) {
+			assert.Equal(t, 1, wallets[0].Id)
+		}
+	})
+}
 
-	{
-		// Create with error
-		testDBCall(db, func(rs app.RequestScope) {
-			wallet := &models.Wallet{
-				Id:           1000,
-				MaximumLimit: 700,
-			}
-			err := dao.Create(rs, wallet)
-			assert.NotNil(t, err)
-			assert.Equal(t, "real_limit: cannot be blank.", err.Error())
-		})
-	}
+func TestCreateWallet(t *testing.T) {
+	db := testdata.ResetDB()
+	dao := NewWalletDAO()
 
-	{
-		// Create with error
-		testDBCall(db, func(rs app.RequestScope) {
-			wallet := &models.Wallet{
-				Id:           1000,
-				MaximumLimit: 700,
-				RealLimit:    0,
-			}
-			err := dao.Create(rs, wallet)
-			assert.NotNil(t, err)
-			assert.Equal(t, "real_limit: cannot be blank.", err.Error())
-		})
-	}
+	testDBCall(db, func(rs app.RequestScope) {
+		wallet := &models.Wallet{
+			MaximumLimit: 500,
+			RealLimit:    200,
+		}
+		err := dao.Create(rs, wallet)
+		assert.Nil(t, err)
+		assert.NotZero(t, wallet.Id)
+	})
+}
 
-	{
-		// Update
-		testDBCall(db, func(rs app.RequestScope) {
-			wallet := &models.Wallet{
-				Id:           1,
-				RealLimit:    234,
-				MaximumLimit: 200,
-			}
-			err := dao.Update(rs, wallet.Id, wallet)
-			assert.Nil(t, err)
-		})
-	}
+func TestCreateWalletWithErrorRealLimitNotBlank(t *testing.T) {
+	db := testdata.ResetDB()
+	dao := NewWalletDAO()
 
-	{
-		// Update with error
-		//Wallet don't belong to authenticated user
-		testDBCall(db, func(rs app.RequestScope) {
-			wallet := &models.Wallet{
-				Id:           2,
-				MaximumLimit: 42,
-				RealLimit:    22,
-			}
-			err := dao.Update(rs, wallet.Id, wallet)
-			assert.NotNil(t, err)
-			assert.Equal(t, "FORBIDDEN", err.Error())
-		})
-	}
+	testDBCall(db, func(rs app.RequestScope) {
+		wallet := &models.Wallet{
+			Id:           1000,
+			MaximumLimit: 700,
+		}
+		err := dao.Create(rs, wallet)
+		assert.NotNil(t, err)
+		assert.Equal(t, "real_limit: cannot be blank.", err.Error())
+	})
+
+}
+
+func TestUpdateWallet(t *testing.T) {
+	db := testdata.ResetDB()
+	dao := NewWalletDAO()
+
+	testDBCall(db, func(rs app.RequestScope) {
+		wallet := &models.Wallet{
+			Id:           1,
+			RealLimit:    234,
+			MaximumLimit: 200,
+		}
+		err := dao.Update(rs, wallet.Id, wallet)
+		assert.Nil(t, err)
+	})
+}
+
+func TestUpdateWalletWithErrorWalletDontBelongToAuthenticatdUser(t *testing.T) {
+	db := testdata.ResetDB()
+	dao := NewWalletDAO()
+
+	testDBCall(db, func(rs app.RequestScope) {
+		wallet := &models.Wallet{
+			Id:           2,
+			MaximumLimit: 42,
+			RealLimit:    22,
+		}
+		err := dao.Update(rs, wallet.Id, wallet)
+		assert.NotNil(t, err)
+		assert.Equal(t, "FORBIDDEN", err.Error())
+	})
+}
+
+func TestWalletDAO(t *testing.T) {
+	db := testdata.ResetDB()
+	dao := NewWalletDAO()
 
 	{
 		// Update with error
