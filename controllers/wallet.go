56b0a03025ea9ffe3d6894858f790cde8a603e8b
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-10-29 23:57:44 -0200

    alterando nome das variaveis para ficar mais coeso

--- controllers/wallet.go
+++ controllers/wallet.go
@@ -12,8 +12,8 @@ type (
 	// walletService especifica a interface que é utilizada pelo walletResource
 	walletService interface {
 		Get(rs app.RequestScope, wallet_id int) (*models.Wallet, error)
-		Create(rs app.RequestScope, model *models.Wallet) (*models.Wallet, error)
-		Update(rs app.RequestScope, wallet_id int, model *models.Wallet) (*models.Wallet, error)
+		Create(rs app.RequestScope, wallet *models.Wallet) (*models.Wallet, error)
+		Update(rs app.RequestScope, wallet_id int, wallet *models.Wallet) (*models.Wallet, error)
 		Delete(rs app.RequestScope, wallet_id int) (*models.Wallet, error)
 	}
 
@@ -38,25 +38,24 @@ func (r *walletResource) Get(c *routing.Context) error {
 		return err
 	}
 
-	response, err := r.service.Get(app.GetRequestScope(c), wallet_id)
+	wallet, err := r.service.Get(app.GetRequestScope(c), wallet_id)
 	if err != nil {
 		return err
 	}
 
-	return c.Write(response)
+	return c.Write(wallet)
 }
 
 func (r *walletResource) Create(c *routing.Context) error {
-	var model models.Wallet
-	if err := c.Read(&model); err != nil {
-		return err
-	}
-	response, err := r.service.Create(app.GetRequestScope(c), &model)
+	tempWallet := models.Wallet{}
+	tempWallet.PersonId = app.GetRequestScope(c).UserID()
+
+	wallet, err := r.service.Create(app.GetRequestScope(c), &tempWallet)
 	if err != nil {
 		return err
 	}
 
-	return c.Write(response)
+	return c.Write(wallet)
 }
 
 func (r *walletResource) Update(c *routing.Context) error {
@@ -67,21 +66,21 @@ func (r *walletResource) Update(c *routing.Context) error {
 
 	rs := app.GetRequestScope(c)
 
-	model, err := r.service.Get(rs, wallet_id)
+	wallet, err := r.service.Get(rs, wallet_id)
 	if err != nil {
 		return err
 	}
 
-	if err := c.Read(model); err != nil {
+	if err := c.Read(wallet); err != nil {
 		return err
 	}
 
-	response, err := r.service.Update(rs, wallet_id, model)
+	newWallet, err := r.service.Update(rs, wallet_id, wallet)
 	if err != nil {
 		return err
 	}
 
-	return c.Write(response)
+	return c.Write(newWallet)
 }
 
 func (r *walletResource) Delete(c *routing.Context) error {
@@ -90,10 +89,10 @@ func (r *walletResource) Delete(c *routing.Context) error {
 		return err
 	}
 
-	response, err := r.service.Delete(app.GetRequestScope(c), wallet_id)
+	wallet, err := r.service.Delete(app.GetRequestScope(c), wallet_id)
 	if err != nil {
 		return err
 	}
 
-	return c.Write(response)
+	return c.Write(wallet)
 }
