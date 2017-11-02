9a15573ba7477b2557662bddb8ff84ec5827d8e4
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-11-02 21:16:02 -0200

    adicionando campo currency ao cartao

--- models/card.go
+++ models/card.go
@@ -13,6 +13,7 @@ type Card struct {
 	CVV             int     `json:"cvv" db:"cc_cvv" description:"Código de verificação do cartão."`
 	RealLimit       float64 `json:"real_limit" db:"cc_real_limit" description:"Limite real do cartão."`
 	AvaliableLimit  float64 `json:"avaliable_limit" db:"cc_avaliable_limit" description:"Limite disponível."`
+	Currency        string  `json:"currency" db:"cc_currency" description:"Tipo de moeda."`
 	WalletId        int     `json:"wallet_id" db:"wallet_id" description:"ID da carteira que esse cartão pertence."`
 }
 
@@ -24,6 +25,7 @@ func (m Card) Validate() error {
 		validation.Field(&m.ExpirationYear, validation.Required),
 		validation.Field(&m.CVV, validation.Required),
 		validation.Field(&m.RealLimit, validation.Required),
+		validation.Field(&m.Currency, validation.Required),
 		validation.Field(&m.AvaliableLimit, validation.Required, validation.Max(float64(*&m.RealLimit))),
 		validation.Field(&m.WalletId, validation.Required),
 	)
--- testdata/db.sql
+++ testdata/db.sql
@@ -34,6 +34,7 @@ CREATE TABLE card
     cc_cvv INTEGER NOT NULL,
     cc_real_limit DECIMAL NOT NULL,
     cc_avaliable_limit DECIMAL NOT NULL,
+    cc_currency VARCHAR(3) NOT NULL,
     wallet_id INTEGER  NOT NULL,
     FOREIGN KEY (wallet_id) REFERENCES wallet (id) ON DELETE CASCADE
 );
@@ -47,13 +48,13 @@ INSERT INTO wallet (current_limit, maximum_limit, avaliable_limit, person_id) VA
 INSERT INTO wallet (current_limit, maximum_limit, avaliable_limit, person_id) VALUES (0, 0, 0, 3);
 INSERT INTO wallet (current_limit, maximum_limit, avaliable_limit, person_id) VALUES (0, 0, 0, 1);
 
-INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, wallet_id) VALUES ('1234123412341230', 10, 06, 17, 123, 300, 180, 1);
-INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, wallet_id) VALUES ('1234123412341231', 10, 06, 17, 123, 400, 200, 1);
-INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, wallet_id) VALUES ('1234123412341232', 11, 08, 16, 123, 500, 450, 1);
-INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, wallet_id) VALUES ('1234123412341233', 12, 10, 14, 123, 600, 480, 2);
-INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, wallet_id) VALUES ('1234123412341234', 12, 01, 14, 123, 300, 120, 2);
-INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, wallet_id) VALUES ('1234123412341235', 13, 02, 15, 123, 400, 350, 2);
-INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, wallet_id) VALUES ('1234123412341236', 14, 03, 15, 123, 500, 480, 2);
-INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, wallet_id) VALUES ('1234123412341237', 15, 04, 16, 123, 600, 360, 3);
-INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, wallet_id) VALUES ('1234123412341238', 16, 05, 17, 123, 700, 670, 3);
-INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, wallet_id) VALUES ('1234123412341239', 17, 06, 16, 123, 800, 600, 3);
+INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, cc_currency, wallet_id) VALUES ('1234123412341230', 10, 06, 17, 123, 300, 180, 'BRL', 1);
+INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, cc_currency, wallet_id) VALUES ('1234123412341231', 10, 06, 17, 123, 400, 200, 'BRL', 1);
+INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, cc_currency, wallet_id) VALUES ('1234123412341232', 11, 08, 16, 123, 500, 450, 'BRL', 1);
+INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, cc_currency, wallet_id) VALUES ('1234123412341233', 12, 10, 14, 123, 600, 480, 'BRL', 2);
+INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, cc_currency, wallet_id) VALUES ('1234123412341234', 12, 01, 14, 123, 300, 120, 'BRL', 2);
+INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, cc_currency, wallet_id) VALUES ('1234123412341235', 13, 02, 15, 123, 400, 350, 'BRL', 2);
+INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, cc_currency, wallet_id) VALUES ('1234123412341236', 14, 03, 15, 123, 500, 480, 'BRL', 2);
+INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, cc_currency, wallet_id) VALUES ('1234123412341237', 15, 04, 16, 123, 600, 360, 'BRL', 3);
+INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, cc_currency, wallet_id) VALUES ('1234123412341238', 16, 05, 17, 123, 700, 670, 'BRL', 3);
+INSERT INTO card (cc_number, cc_due_date, cc_expiration_month, cc_expiration_year, cc_cvv, cc_real_limit, cc_avaliable_limit, cc_currency, wallet_id) VALUES ('1234123412341239', 17, 06, 16, 123, 800, 600, 'BRL', 3);
